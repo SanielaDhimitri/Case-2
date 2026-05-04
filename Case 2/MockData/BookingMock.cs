8b8e0033f7e3b632ce774c88f43a29fe4b260044
malindatahiri <malindatahiri21@gmail.com>
2026-05-04 12:25:18 +0200

    update my code

---
+++ Case 2/MockData/BookingMock.cs
@@ -0,0 +1,32 @@
+using Case_2.Models;
+
+namespace Case_2.MockData
+{
+    public class BookingMock
+    {
+        public static List<Booking> GetBookings()
+        {
+            return new List<Booking>
+            {
+                new Booking
+                {
+                    BookingId = 1,
+                    RoomId = 101,
+                    UserId = 1,
+                    TimeSlot = new TimeSlot(1, "08:00", "10:00"),
+                    Date = DateTime.Today,
+                    Status = true
+                },
+                new Booking
+                {
+                    BookingId = 2,
+                    RoomId = 102,
+                    UserId = 2,
+                    TimeSlot = new TimeSlot(2, "10:00", "12:00"),
+                    Date = DateTime.Today,
+                    Status = true
+                }
+            };
+        }
+    }
+}
